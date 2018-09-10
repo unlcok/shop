787a8d8f560c108c53a9b993b78aef375b11a1a5
甄文康 <zhenwenkang@icourt.cc>
2018-09-10 21:11:52 +0800

    first commit

---
+++ shop-provider/src/main/java/com/test/shopprovider/ShopProviderApplication.java
@@ -0,0 +1,12 @@
+package com.test.shopprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopProviderApplication.class, args);
+    }
+}
---
+++ shop-web/src/main/java/com/test/shopweb/ShopWebApplication.java
@@ -0,0 +1,12 @@
+package com.test.shopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopWebApplication.class, args);
+    }
+}
